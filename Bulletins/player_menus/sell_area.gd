d814226681dcdb7f83dd644823342543e63ae436
Yolo-Arts <Jackteong123@gmail.com>
2026-04-25 20:46:07 +0100

    items sell for value

--- Bulletins/player_menus/sell_area.gd
+++ Bulletins/player_menus/sell_area.gd
@@ -7,5 +7,11 @@ func _can_drop_data(_at_position: Vector2, slot: Variant) -> bool:
 
 func _drop_data(_at_position: Vector2, old_slot: Variant) -> void:
 	if old_slot is InventorySlot:
-		#EventSystem.
+		var key = old_slot.item_key
+		
+		var price = ItemConfig.get_item_resource(key).sell_price
+		
+		EventSystem.MON_add_money.emit(price)
+		
+		EventSystem.INV_delete_item_by_index.emit(old_slot.get_index())
 		item_scrapped.emit()
