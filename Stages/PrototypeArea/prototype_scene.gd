7c1193f3da0d89649f722ad033272f04109d8f02
Yolo-Arts <Jackteong123@gmail.com>
2026-04-27 12:33:08 +0100

    added randomised tree spawning on game begin

--- Stages/PrototypeArea/prototype_scene.gd
+++ Stages/PrototypeArea/prototype_scene.gd
@@ -1,4 +1,20 @@
-extends Node3D
+extends Node
+
+@onready var scatter_trees: ProtonScatter = $NavigationRegion3D/ScatterTrees
+@onready var navigation_region_3d: NavigationRegion3D = $NavigationRegion3D
 
 func _ready() -> void:
 	EventSystem.MUS_play_music.emit(MusicConfig.Keys.IslandAmbience)
+	
+	await get_tree().process_frame
+	
+	randomize_forest()
+	scatter_trees.build_completed.connect(_on_scatter_finished)
+
+func randomize_forest() -> void:
+	if scatter_trees:
+		scatter_trees.global_seed = randi_range(0, 60)
+		print("Forest randomized with seed: ", scatter_trees.global_seed)
+
+func _on_scatter_finished() -> void:
+	navigation_region_3d.bake_navigation_mesh()
