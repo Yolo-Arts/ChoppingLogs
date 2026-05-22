24a584db887d95a68ffe57f3f289e823d2027f6b
Yolo-Arts <Jackteong123@gmail.com>
2026-05-22 19:58:14 +0100

    random optimisation push

--- Objects/interactables/pickuppables/rigid_pickuppable_log.gd
+++ Objects/interactables/pickuppables/rigid_pickuppable_log.gd
@@ -1,11 +1,6 @@
 extends RigidBody3D
 class_name PickuppableLog
-
-@onready var pickuppable_component: Pickuppable = $Area3D
-
-func start_interaction() -> void:
-	if pickuppable_component:
-		pickuppable_component.start_interaction()
+@export var item_key : ItemConfig.Keys
 
 func _ready() -> void:
 	sleeping_state_changed.connect(_on_sleeping_state_changed)
@@ -13,3 +8,27 @@ func _ready() -> void:
 func _on_sleeping_state_changed() -> void:
 	if sleeping:
 		freeze = true
+
+var player_reference: Node3D = null
+
+func start_interaction() -> void:
+	player_reference = get_tree().get_first_node_in_group("player")
+	EventSystem.INV_try_to_pickup_item.emit(item_key, destroy)
+
+func destroy() -> void:
+	if not player_reference:
+		ObjectPool.return_object(self)
+		return
+
+	var tween = create_tween().set_parallel(true)
+	
+	tween.set_trans(Tween.TRANS_BACK).set_ease(Tween.EASE_IN)
+
+	var target_position = player_reference.global_position + Vector3(0, 1.6, 0)
+
+	tween.tween_property(self, "global_position", target_position, 0.8)
+	tween.tween_property(self, "scale", Vector3(0.001, 0.001, 0.001), 0.8)
+
+	tween.chain().tween_callback(func():
+		ObjectPool.return_object(self)
+	)
