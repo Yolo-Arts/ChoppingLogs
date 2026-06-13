62a6e6bd478260fcea54c500c0859397ee931faf
thestrongest9 <55645898+thestrongest9@users.noreply.github.com>
2026-06-12 23:53:41 -0700

    Use Damage object when spawning damage number UI text.

--- Particles/damage_numbers/damage_numbers.gd
+++ Particles/damage_numbers/damage_numbers.gd
@@ -2,11 +2,31 @@ extends Sprite3D
 
 @onready var label: Label = %Label
 
-func display_damage():
+# TODO: There must be some way to re-use the 
+#		function below from hud.gd
+func apply_text_effect(label: Label, color: Color):
+	# var color_chain = create_tween()
+	# Yellow -> White
+	# color_chain.tween_property(label, "theme_override_colors/font_color", color, 0.1)
+	# color_chain.tween_property(label, "theme_override_colors/font_color", Color.WHITE, 0.3).set_delay(0.1)
+	# White -> Yellow
+	# color_chain.tween_property(label, "theme_override_colors/font_color", Color.WHITE, 0.1)
+	# color_chain.tween_property(label, "theme_override_colors/font_color", color, 0.3).set_delay(0.1)
+	# Set color to Yellow.
+	label.add_theme_color_override("font_color", color)
+
+func display_damage(dmg: Damage) -> void:
+	if dmg == null:	# return if damage is missing
+		return
+
 	var final_pos = global_position + Vector3(randf_range(-0.5, 0.5), randf_range(-0.5, 0.5), 0) 
 	scale = Vector3.ZERO
 	modulate.a = 1.0
 	
+	# Set color to yellow on crit
+	if label and dmg.crit:	# check if label exists + crit.
+		apply_text_effect(label, Color.YELLOW)
+
 	var tween = create_tween()
 	tween.set_parallel(true)
 	
