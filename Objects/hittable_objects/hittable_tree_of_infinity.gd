48bebb8e1a1e92c3b7db3daf382cebcc561dd932
LifeHckr <jarodthereal@gmail.com>
2026-06-17 17:25:12 -0700

    add regeneration effect to TOI

--- Objects/hittable_objects/hittable_tree_of_infinity.gd
+++ Objects/hittable_objects/hittable_tree_of_infinity.gd
@@ -5,3 +5,24 @@ func _ready() -> void:
 
 func die() -> void:
 	super()
+	
+func _process(delta: float) -> void:
+	regen(delta)
+	
+#region Regen
+const REGEN_TICK_TIME: float = 1.0
+var time_accumulation: float = 0.0
+func regen(delta : float) -> void:
+	if current_health >= attributes.max_health:
+		return
+		
+	time_accumulation +=  delta
+	if time_accumulation < REGEN_TICK_TIME:
+		return
+		
+	current_health = clamp(current_health + attributes.regeneration, 0, attributes.max_health)
+	
+	time_accumulation = (time_accumulation - REGEN_TICK_TIME)
+	if current_health >= attributes.max_health:
+		time_accumulation = 0
+#endregion
