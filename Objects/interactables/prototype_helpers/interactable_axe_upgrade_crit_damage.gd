802e6ce9c19be215684d29074f527097f1e936d3
Yolo-Arts <Jackteong123@gmail.com>
2026-06-23 13:53:59 +0100

    adding placeholder map

--- Objects/interactables/prototype_helpers/interactable_axe_upgrade_crit_damage.gd
+++ Objects/interactables/prototype_helpers/interactable_axe_upgrade_crit_damage.gd
@@ -1,5 +1,5 @@
 extends Interactable
 
 func start_interaction() -> void:
-    # TODO: How much should each interaction increase crit_chance
-    EventSystem.UPG_increase_crit_damage.emit(5.0)
+	# TODO: How much should each interaction increase crit_chance
+	EventSystem.UPG_increase_crit_damage.emit(5.0)
