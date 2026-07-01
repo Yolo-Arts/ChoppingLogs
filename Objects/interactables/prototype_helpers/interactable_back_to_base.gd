56b1b2a73dec92dee9a777b8e2e3b822929f6e42
Yolo-Arts <Jackteong123@gmail.com>
2026-07-01 15:24:15 +0100

    linking the new results screen to game

--- Objects/interactables/prototype_helpers/interactable_back_to_base.gd
+++ Objects/interactables/prototype_helpers/interactable_back_to_base.gd
@@ -3,6 +3,7 @@ extends Interactable
 func start_interaction() -> void:
 	var end_day = EventSystem.QUO_check_quota.call()
 	if end_day:
-		EventSystem.BUL_create_bulletin.emit(BulletinConfig.Keys.PlaceholderDayEnd)
+		EventSystem.BUL_create_bulletin.emit(BulletinConfig.Keys.ResultsScreen)
+		EventSystem.BUL_destroy_bulletin.emit(BulletinConfig.Keys.InteractionPrompt)
 	else:
 		print("not enough money")
