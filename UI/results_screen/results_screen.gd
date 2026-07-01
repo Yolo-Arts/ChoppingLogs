5424b54dd2c037cadf9d5a983934b48f6a61a204
Yolo-Arts <Jackteong123@gmail.com>
2026-07-01 14:50:09 +0100

    utilising shaker and tweening buttons

--- UI/results_screen/results_screen.gd
+++ UI/results_screen/results_screen.gd
@@ -13,6 +13,9 @@ extends Bulletin
 @onready var prestige_points_lbl: Label = %PrestigePointsLbl
 @onready var label_categories: VBoxContainer = %LabelCategories
 @onready var label_numbers: VBoxContainer = %LabelNumbers
+@onready var shaker: Shaker = $Shaker
+@onready var continue_button: Button = %ContinueButton
+@onready var back_to_menu: TextureButton = %BackToMenu
 
 var quota = 500
 
@@ -22,6 +25,7 @@ func _ready() -> void:
 	get_tree().paused = true
 	
 	hide_labels()
+	hide_buttons()
 	
 	var tween = create_tween()
 	tween.set_trans(Tween.TRANS_BACK)
@@ -41,6 +45,10 @@ func hide_labels():
 	for child in label_numbers.get_children():
 		child.self_modulate.a = 0.0
 
+func hide_buttons():
+	for child in $MarginContainer/ResultsContainer/VBoxContainer/ButtonsContainer.get_children():
+		child.modulate.a = 0.0
+
 func animate_stats():
 	var tween: Tween = create_tween().set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_CUBIC)
 	
@@ -54,27 +62,41 @@ func animate_stats():
 	#FIXLATER the values are purely visual
 	tween.tween_method(set_label_number.bind(trees_cut_count_lbl), 0, 143, 0.5)
 	tween.parallel().tween_property(trees_cut_count_lbl, "self_modulate:a", 1.0, 0.05).from(0.0)
+	tween.parallel().tween_callback(shaker.start.bind(0.25))
 	tween.tween_interval(0.25)
 	
 	tween.tween_method(set_label_number.bind(quota_number_lbl), 0, -2228, 0.5)
 	tween.parallel().tween_property(quota_number_lbl, "self_modulate:a", 1.0, 0.05).from(0.0)
+	tween.parallel().tween_callback(shaker.start.bind(0.25))
 	tween.tween_interval(0.25)
 	
 	tween.tween_method(set_label_number.bind(todays_revenue_number_lbl), 0, 8900, 0.5)
 	tween.parallel().tween_property(todays_revenue_number_lbl, "self_modulate:a", 1.0, 0.05).from(0.0)
+	tween.parallel().tween_callback(shaker.start.bind(0.25))
 	tween.tween_interval(0.25)
 	
 	tween.tween_method(set_label_number.bind(balance_number_lbl), 0, 8900 - 2228, 0.5)
 	tween.parallel().tween_property(balance_number_lbl, "self_modulate:a", 1.0, 0.05).from(0.0)
+	tween.parallel().tween_callback(shaker.start.bind(0.25))
 	tween.tween_interval(0.25)
 	
 	tween.tween_method(set_label_number.bind(prestige_points_number_lbl), 0, 178, 0.5)
 	tween.parallel().tween_property(prestige_points_number_lbl, "self_modulate:a", 1.0, 0.05).from(0.0)
+	tween.parallel().tween_callback(shaker.start.bind(0.25))
 	tween.tween_interval(0.25)
+	
+	tween.tween_callback(transition_buttons)
 	
 
 func set_label_number(number: int, label: Label) -> void:
 	label.set_text(str(number))
+
+func transition_buttons():
+	var tween: Tween = create_tween().set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_CUBIC)
+	tween.tween_property(continue_button, "position:y", 0, 0.5).from(1200)
+	tween.parallel().tween_property(continue_button, "modulate:a", 1, 0.1).from(0.0)
+	tween.parallel().tween_property(back_to_menu, "position:y", 0, 0.5).from(1200)
+	tween.parallel().tween_property(back_to_menu, "modulate:a", 1, 0.1).from(0.0)
 
 func _on_continue_button_pressed() -> void:
 	get_tree().paused = false
