ce87723e62ed6577deb96a6392eb004e723f6646
Yolo-Arts <Jackteong123@gmail.com>
2026-07-01 17:23:46 +0100

    making overflow progress bar appear

--- Objects/interactables/prototype_helpers/interactable_money_increase.gd
+++ Objects/interactables/prototype_helpers/interactable_money_increase.gd
@@ -1,4 +1,6 @@
 extends Interactable
 
+@export var money: int = 500
+
 func start_interaction() -> void:
-	EventSystem.MON_add_money.emit(5)
+	EventSystem.MON_add_money.emit(money)
--- UI/results_screen/results_screen.gd
+++ UI/results_screen/results_screen.gd
@@ -16,6 +16,17 @@ extends Bulletin
 @onready var shaker: Shaker = $Shaker
 @onready var continue_button: Button = %ContinueButton
 @onready var back_to_menu: TextureButton = %BackToMenu
+@onready var quota_percentage: Label = %QuotaPercentage
+@onready var quota_progress_bar: ProgressBar = %QuotaProgressBar
+@onready var overflow_container: VBoxContainer = %OverflowContainer
+@onready var quota_overflow_percentage: Label = %QuotaOverflowPercentage
+@onready var quota_overflow_progress_bar: ProgressBar = %QuotaOverflowProgressBar
+@onready var progress_bar_h_split_container: HSplitContainer = %ProgressBarHSplitContainer
+
+var trees_cut = EventSystem.TRE_get_tree_cut_amount.call()
+var quota = EventSystem.QUO_get_quota_amount.call()
+var todays_revenue = EventSystem.MON_get_player_money.call()
+var progress_bar_width = 818
 
 func _ready() -> void:
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
@@ -24,6 +35,8 @@ func _ready() -> void:
 	
 	hide_labels()
 	hide_buttons()
+	quota_overflow_progress_bar.visible = false
+	overflow_container.visible = false
 	
 	var tween = create_tween()
 	tween.set_trans(Tween.TRANS_BACK)
@@ -48,6 +61,7 @@ func hide_buttons():
 		child.modulate.a = 0.0
 
 func animate_stats():
+	fill_quota_progress_bar()
 	var tween: Tween = create_tween().set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_CUBIC)
 	
 	for child in label_categories.get_children():
@@ -56,9 +70,6 @@ func animate_stats():
 	
 	tween.tween_interval(0.25)
 	
-	var trees_cut = EventSystem.TRE_get_tree_cut_amount.call()
-	var quota = EventSystem.QUO_get_quota_amount.call()
-	var todays_revenue = EventSystem.MON_get_player_money.call()
 	
 	tween.tween_method(set_label_number.bind(trees_cut_count_lbl), 0, trees_cut, 0.5)
 	tween.parallel().tween_property(trees_cut_count_lbl, "self_modulate:a", 1.0, 0.05).from(0.0)
@@ -75,6 +86,7 @@ func animate_stats():
 	tween.parallel().tween_callback(shaker.start.bind(0.25))
 	tween.tween_interval(0.25)
 	
+	
 	tween.tween_method(set_label_number.bind(balance_number_lbl), 0, todays_revenue - quota, 0.5)
 	tween.parallel().tween_property(balance_number_lbl, "self_modulate:a", 1.0, 0.05).from(0.0)
 	tween.parallel().tween_callback(shaker.start.bind(0.25))
@@ -86,7 +98,6 @@ func animate_stats():
 	tween.tween_interval(0.25)
 	
 	tween.tween_callback(transition_buttons)
-	
 
 func set_label_number(number: int, label: Label) -> void:
 	match label:
@@ -101,6 +112,45 @@ func set_label_number(number: int, label: Label) -> void:
 			label.set_text(str(number) + "p")
 		_:
 			label.set_text(str(number))
+
+func fill_quota_progress_bar():
+	var quota_percentage_tween: Tween = create_tween()
+	quota_percentage_tween.tween_method(func(v): quota_percentage.text = "%.f%%" % v, 0.0, min((todays_revenue / float(quota)) * 100.0, 100), 2.5)
+	
+	quota_progress_bar.max_value = quota
+	quota_overflow_progress_bar.visible = false
+	quota_overflow_progress_bar.custom_minimum_size.x = 0
+	quota_progress_bar.custom_minimum_size.x = 0
+	overflow_container.visible = false
+	
+	const SPLIT_MIN = -360.0
+	const SPLIT_MAX = 360.0
+	progress_bar_h_split_container.split_offset = SPLIT_MAX
+	progress_bar_h_split_container.custom_minimum_size.x = progress_bar_width
+	progress_bar_h_split_container.clip_contents = true
+	
+	var overflow_amount = todays_revenue - quota
+	
+	var bar_tween: Tween = create_tween().set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_CUBIC)
+	bar_tween.tween_property(quota_progress_bar, "value", quota, 2.5).from(0)
+	
+	if overflow_amount > 0:
+		var total_amount = quota + overflow_amount
+		var overflow_ratio = float(overflow_amount) / total_amount
+		var target_split_offset = lerp(SPLIT_MAX, SPLIT_MIN, overflow_ratio)
+		
+		var overflow_percentage = (float(overflow_amount) / quota) * 100.0
+		
+		quota_overflow_progress_bar.max_value = overflow_amount
+		quota_overflow_progress_bar.value = quota_overflow_progress_bar.max_value
+		
+		bar_tween.tween_callback(func():
+			overflow_container.visible = true
+			quota_overflow_progress_bar.visible = true
+		)
+		
+		bar_tween.tween_property(progress_bar_h_split_container, "split_offset", target_split_offset, 4.5)
+		bar_tween.parallel().tween_method(func(v): quota_overflow_percentage.text = "%.f%%" % v, 0.0, overflow_percentage, 4.5)
 
 func transition_buttons():
 	var tween: Tween = create_tween().set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_CUBIC)
