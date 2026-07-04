70e663cb97e59b177fbe6ae75bbe0e4731ef3ee0
LifeHckr <jarodthereal@gmail.com>
2026-07-04 14:11:34 -0700

    finish timer increase upgrades

--- AutoLoads/event_system.gd
+++ AutoLoads/event_system.gd
@@ -34,6 +34,7 @@ signal HUD_hide_hud
 signal HUD_show_hud
 signal HUD_reset_hud_elements
 signal HUD_update_time
+signal HUD_change_countdown(amount: float)
 
 # INV -> Inventory signals
 signal INV_ask_update_inventory
