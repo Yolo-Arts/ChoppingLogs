aca53dba8cd38a270b8342b73a4e658c33142341
Yolo-Arts <Jackteong123@gmail.com>
2026-07-18 22:23:29 +0100

    playtesting 2 by yolo

--- UI/skill_tree/skill_tree.gd
+++ UI/skill_tree/skill_tree.gd
@@ -38,7 +38,7 @@ func _close_skill_tree() -> void:
 
 func _unhandled_key_input(event: InputEvent) -> void:
 	if event.is_action_pressed("close_menu"):
-		_close_skill_tree()
+		#_close_skill_tree()
 		pass
 
 func _run_ripple_cascade(current_node: Node, current_delay: float) -> void:
